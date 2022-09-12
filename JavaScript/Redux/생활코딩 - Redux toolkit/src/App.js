1416585890f9eb84ebe9297c538b36a0e7030fd0
nmin11 <nmin1124@gmail.com>
2022-09-12 15:33:37 +0900

    Add: redux toolkit lecture

---
+++ JavaScript/Redux/생활코딩 - Redux toolkit/src/App.js
@@ -0,0 +1,25 @@
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import store from './store';
+import { up } from './counterSlice';
+
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector(state => state.counter.value);
+
+  return (
+    <div>
+      <button onClick={() => dispatch(up(2))}>+</button>
+      {count}
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <Counter />
+      </div>
+    </Provider>
+  )
+}
